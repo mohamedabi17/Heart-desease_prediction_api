08e47e0cc6073acb2cbed6cba047004232e1ee7a
mohamedabi17 <mohamedabi1485@gmail.com>
2023-11-27 02:28:37 +0100

    27nov

--- flaskapi/app.py
+++ flaskapi/app.py
@@ -2,9 +2,17 @@
 from flask import Flask,render_template, request, jsonify
 from models.randomforest_model import load_model
 import numpy as np
+import pandas as pd
 
 app = Flask(__name__, template_folder='./templates')
 randomforest_model = load_model()  # Load the model outside the route function
+
+
+dataset_path = './models/dataset.csv'
+data = pd.read_csv(dataset_path)
+X_train = data.drop('target', axis=1)  # Adjust the column name according to your dataset
+y_train = data['target']  # Adjust the column name according to your dataset
+randomforest_model.fit(X_train, y_train)
 
 
 @app.route('/')
@@ -17,10 +25,12 @@ def predict():
         # Get input data from the request
         data = request.get_json()
 
-        # Extract features from the input data
+        # Convert relevant features to float
         features = [
-            data['age'], 'trestbps', 'chol', 'thalach', 'oldpeak', 'sex_0', 'sex_1',
-            'cp_0', 'cp_1', ..., 'thal_0', 'thal_1', 'thal_2', 'thal_3'
+            float(data['age']), float(data['trestbps']), float(data['chol']),
+            float(data['thalach']), float(data['oldpeak']), float(data['sex_0']),
+            float(data['sex_1']), float(data['cp_0']), float(data['cp_1']),
+            float(data['thal_0']), float(data['thal_1']), float(data['thal_2']), float(data['thal_3'])
         ]
 
         # Convert features to a NumPy array for prediction
@@ -34,5 +44,6 @@ def predict():
 
     except Exception as e:
         return jsonify({'error': str(e)})
+
 if __name__ == '__main__':
     app.run(debug=True)
